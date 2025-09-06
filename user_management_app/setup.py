e20ea658cae420bbd75ce5858cc160cefcc6987d
Ashraf-Khabar <khabarachraf@gmail.com>
2025-09-06 13:49:29 +0200

    Ajout des nouveaux fichiers

--- user_management_app/setup.py
+++ user_management_app/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="user_management_app",
-    version="2.0.0",
+    version="3.0.0",
     packages=find_packages(),
     install_requires=[
         "fastapi>=0.104.1",
