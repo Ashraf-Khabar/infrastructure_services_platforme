949c3ebbb9fa35f0646896c9444158db6fb077ab
Ashraf-Khabar <khabarachraf@gmail.com>
2025-09-13 14:54:36 +0200

    Ajout des nouveaux fichiers - 2025-09-13 14:54:35

--- cloud_infrastructure/main.tf
+++ cloud_infrastructure/main.tf
@@ -150,34 +150,3 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
-
-# Outputs
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_eip.app.public_ip
-}
-
-output "instance_private_ip" {
-  description = "Private IP address of the EC2 instance"
-  value       = aws_instance.app.private_ip
-}
-
-output "application_url" {
-  description = "URL of the deployed application"
-  value       = "http://${aws_eip.app.public_ip}:5000"
-}
-
-output "ssh_connection_command" {
-  description = "SSH connection command"
-  value       = "ssh -i ~/.ssh/id_rsa ubuntu@${aws_eip.app.public_ip}"
-}
-
-output "vpc_id" {
-  description = "ID of the VPC used"
-  value       = data.aws_vpc.existing.id
-}
-
-output "subnet_id" {
-  description = "ID of the subnet used"
-  value       = data.aws_subnet.selected.id
-}
--- cloud_infrastructure/outputs.tf
+++ cloud_infrastructure/outputs.tf
@@ -12,3 +12,13 @@ output "ssh_connection_command" {
   description = "SSH connection command"
   value       = "ssh -i ~/.ssh/id_rsa ubuntu@${aws_eip.app.public_ip}"
 }
+
+output "vpc_id" {
+  description = "ID of the VPC used"
+  value       = data.aws_vpc.existing.id
+}
+
+output "subnet_id" {
+  description = "ID of the subnet used"
+  value       = data.aws_subnet.selected.id
+}
