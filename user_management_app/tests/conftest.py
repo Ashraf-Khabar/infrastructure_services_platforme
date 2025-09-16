b16fb13cba61d29a95422fc14abb361d04a14e26
Ashraf-Khabar <khabarachraf@gmail.com>
2025-09-16 21:16:57 +0200

    Ajout des nouveaux fichiers - 2025-09-16 21:16:57

--- user_management_app/tests/conftest.py
+++ user_management_app/tests/conftest.py
@@ -6,16 +6,6 @@ def api_url():
     return "http://localhost:5002"
 
 @pytest.fixture
-    return {
-        "username": "testuser",
-        "email": "test@example.com",
-        "password": "testpassword",
-        "first_name": "Test",
-        "last_name": "User",
-        "role": "user"
-    }
-
-@pytest.fixture
 def admin_credentials():
     return {
         "username": "admin",
